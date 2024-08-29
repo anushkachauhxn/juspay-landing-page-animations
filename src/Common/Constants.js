0503a36225a8749b01528b96d3199ba6e10b804c
Anushka Chauhan <anushka.chauhan@mygate.in>
2024-08-29 23:44:27 +0530

    Added assets for brands

--- src/Common/Constants.js
+++ src/Common/Constants.js
@@ -1,8 +1,22 @@
-import seaIcon from "../Assets/sea.svg";
-import latamIcon from "../Assets/latam.svg";
-import northAmericaIcon from "../Assets/north-america.svg";
-import europeIcon from "../Assets/europe.svg";
-import indiaIcon from "../Assets/india.svg";
+// globe icons
+import seaIcon from "../Assets/globe_icons/sea.svg";
+import latamIcon from "../Assets/globe_icons/latam.svg";
+import northAmericaIcon from "../Assets/globe_icons/north-america.svg";
+import europeIcon from "../Assets/globe_icons/europe.svg";
+import indiaIcon from "../Assets/globe_icons/india.svg";
+// brand icons
+import agodaIcon from "../Assets/brands/agoda.svg";
+import flipkartIcon from "../Assets/brands/flipkart.svg";
+import walmartIcon from "../Assets/brands/walmart.svg";
+import amazonIcon from "../Assets/brands/amazon.svg";
+import googleIcon from "../Assets/brands/google.svg";
+import microsoftIcon from "../Assets/brands/microsoft.svg";
+import oneplusIcon from "../Assets/brands/oneplus.svg";
+import indigoIcon from "../Assets/brands/indigo.svg";
+import mcdonaldsIcon from "../Assets/brands/mcdonalds.svg";
+import burgerkingIcon from "../Assets/brands/burgerking.svg";
+import xiaomiIcon from "../Assets/brands/xiaomi.svg";
+import starbucksIcon from "../Assets/brands/starbucks.svg";
 
 export const GLOBAL_MENU_OPTIONS = [
   {
@@ -48,4 +62,19 @@ export const GLOBAL_OUTCOMES_ANIMATION_LETTERS = [
   ["¥","€","£","M"],
   ["R","¥","₹","E"],
   ["¥","R","£","S"]
+];
+
+export const BRANDS_LIST = [
+  { name: "agoda", icon: agodaIcon },
+  { name: "flipkart", icon: flipkartIcon },
+  { name: "walmart", icon: walmartIcon },
+  { name: "amazon", icon: amazonIcon },
+  { name: "google", icon: googleIcon },
+  { name: "microsoft", icon: microsoftIcon },
+  { name: "oneplus", icon: oneplusIcon },
+  { name: "indigo", icon: indigoIcon },
+  { name: "mcdonalds", icon: mcdonaldsIcon },
+  { name: "burgerking", icon: burgerkingIcon },
+  { name: "xiaomi", icon: xiaomiIcon },
+  { name: "starbucks", icon: starbucksIcon },
 ];
