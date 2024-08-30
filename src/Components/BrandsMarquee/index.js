9c17fbfeb4c362bafac6fe9f449c8b82846d6711
Anushka Chauhan <anushka.chauhan@mygate.in>
2024-08-30 17:23:56 +0530

    Brands Marquee faded edges

--- src/Components/BrandsMarquee/index.js
+++ src/Components/BrandsMarquee/index.js
@@ -11,17 +11,17 @@ const BrandsMarquee = () => {
   gsap.registerPlugin(useGSAP);
   const container = useRef();
   useGSAP(() => {
-    let tl = gsap.timeline({ delay: 0, repeat: -1, ease: "none" });
+    let tl = gsap.timeline({ delay: 0, repeat: -1, ease: "sine.inOut" });
 
     tl.to(".brands-list", {
       x: "-100%",
-      duration: 60
+      duration: 35
     });
   }, { scope: container });
 
   return (
     <div className="brands-marquee" ref={container}>
-      <div className="brands" ref={container}>
+      <div className="brands faded faded-left faded-right" ref={container}>
         <div className="brands-list">
           {BRANDS_LIST.map((brand, index) => (
             <div className="brand" key={index}>
